550ce6e17889a12d65c650e6cbf8bd05321f4294
zwel-htet-yan <zwelhtetyan2001@gmail.com>
2022-03-24 22:49:44 +0630

    test1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const images = [
+    'img1.jpg',
+    'img2.jpg',
+    'img3.jpeg',
+    'img4.jpeg',
+    'img5.jpeg',
+    'img6.jpeg',
+    'img7.jpeg',
+    'img8.jpeg',
+    'img9.jpg',
+    'img10.jpeg',
+    'img11.jpeg',
+    'img12.jpeg',
+    'img13.jpeg',
+    'img14.jpeg',
+    'img15.jpeg',
+    'img16.jpeg',
+];
+
+const imgContainer = document.querySelector('#img-container');
