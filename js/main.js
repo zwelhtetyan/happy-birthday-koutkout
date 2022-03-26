58e206f3378101c60de9994483e033a8765fc511
zwel-htet-yan <zwelhtetyan2001@gmail.com>
2022-03-26 17:01:59 +0630

    tgallery

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ const images = [
     'img5.jpeg',
     'img6.jpeg',
     'img7.jpeg',
-    'img8.jpeg',
-    'img9.jpg',
-    'img10.jpeg',
-    'img11.jpeg',
     'img12.jpeg',
     'img13.jpeg',
+    'img10.jpeg',
+    'img11.jpeg',
+    'img8.jpeg',
+    'img9.jpg',
     'img14.jpeg',
     'img15.jpeg',
     'img16.jpeg',
@@ -43,6 +43,7 @@ const box = document.querySelector('.gift-box');
 const giftTxt = document.querySelector('.gift-txt');
 const animateLayer = document.querySelector('.animate-layer');
 
+//handle click on box
 box.addEventListener('click', () => {
     if (box.classList.contains('open-box')) return;
     box.src = './assets/gift-cake1.png';
@@ -67,4 +68,33 @@ box.addEventListener('click', () => {
 const backArrow = document.querySelector('.back-arrow');
 backArrow.addEventListener('click', () => {
     giftContainer.style.display = 'none';
+});
+
+//display images
+const imgCollection = document.querySelector('.img-collection');
+const imagesToShow = images
+    .map((img, idx) => {
+        return `<div class="img-container mb-4">
+                <div class="d-flex align-items-center justify-content-center">
+                    <img src="./gallery/${img}" alt="" class='kout-img' id="${idx}">
+                </div>
+                </div>`;
+    })
+    .join('');
+
+imgCollection.innerHTML = imagesToShow;
+
+//view image
+const koutImg = document.querySelectorAll('.kout-img');
+const viewImageLayer = document.querySelector('.view-img-layer');
+const overlayLayer = document.querySelector('.overlay-layer');
+const viewImage = document.querySelector('.view-img');
+
+koutImg.forEach((img) => {
+    img.addEventListener('click', (e) => {
+        const id = e.currentTarget.id;
+        viewImage.src = `./gallery/${images[id]}`;
+        viewImageLayer.style.transform = 'scale(1)';
+        overlayLayer.classList.add('open-overlay');
+    });
 });
