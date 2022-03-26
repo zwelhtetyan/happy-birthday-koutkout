176554c24d1e79bfae6d7ae720142f7847b5a981
zwel-htet-yan <zwelhtetyan2001@gmail.com>
2022-03-26 12:28:42 +0630

    t

--- js/main.js
+++ js/main.js
@@ -42,6 +42,7 @@ logo.addEventListener('click', () => (giftContainer.style.display = 'flex'));
 const box = document.querySelector('.gift-box');
 const giftTxt = document.querySelector('.gift-txt');
 const animateLayer = document.querySelector('.animate-layer');
+const backgroundLayer = document.querySelector('.background-layer');
 
 box.addEventListener('click', () => {
     box.src = './assets/gift-cake1.png';
@@ -49,7 +50,7 @@ box.addEventListener('click', () => {
         'This cake is for you <span style="font-style: normal;">🤍</span>';
     giftTxt.classList.remove('animate-active');
     giftTxt.style.opacity = '0';
-    giftContainer.style.background = 'url(../assets/black-bollons.svg)';
+    backgroundLayer.style.display = 'block';
     setTimeout(() => {
         giftTxt.style.opacity = '1';
         giftTxt.classList.add('animate__animated', 'animate__shakeX');
